2591ffa080f161591e22d4b4e4b97d885dcdd025
KelloHitt <venus.cert@gmail.com>
2026-04-24 20:17:09 +1200

    Changed css styling

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -12,13 +12,12 @@ function App() {
     
     <div class="app-name">
       <div class="landing-page-title">
-      <h1>Task Mitram</h1>
+      <h1>Task Mitram</h1> 
       <h3 id="translation">मित्रम् &bull; mitram &bull; companion</h3>
       </div>
     </div>
     Image by <a href="https://pixabay.com/users/pexels-2286921/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1867636">Pexels</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1867636">Pixabay</a>
-    <p>Acknowledgement: Photo by <a href="https://unsplash.com/@wesleyphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wesley Tingey</a> on <a href="https://unsplash.com/photos/rippling-crystal-blue-water-9-OCsKoyQlk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-      </p>
+
     </>
   )
 }
